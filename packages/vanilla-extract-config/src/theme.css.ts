2b85ceead7b432ee23000df7904a057eba6c056d
Tessa1217 <kwonyuj1996@gmail.com>
2025-10-05 00:52:14 +0900

    docs(tokens): 크기 관련 문서 수정

--- packages/vanilla-extract-config/src/theme.css.ts
+++ packages/vanilla-extract-config/src/theme.css.ts
@@ -1,6 +1,4 @@
 import { createThemeContract } from "@vanilla-extract/css";
 import { themeTokens } from "@packages/tokens/vanilla-extract";
 
-// Create a contract with the shape of the tokens.
-// This doesn't generate any CSS, just a typed object of variable names.
 export const vars = createThemeContract(themeTokens);
