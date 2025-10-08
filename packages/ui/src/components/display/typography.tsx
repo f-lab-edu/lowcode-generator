a1dc11ba1b203a362c1ff4706a79af26fc53f4bf
Tessa1217 <kwonyuj1996@gmail.com>
2025-10-08 18:10:27 +0900

    feat(atomic components): Typography 컴포넌트 생성 및 storybook component 문서화 추가

---
+++ packages/ui/src/components/display/typography.tsx
@@ -0,0 +1,60 @@
+import {
+  forwardRef,
+  type ElementType,
+  type ComponentPropsWithRef,
+  type ComponentPropsWithoutRef,
+} from "react";
+import {
+  typographyRecipe,
+  type TypographyRole,
+} from "@packages/vanilla-extract-config";
+
+type TypographyElement =
+  | "p"
+  | "span"
+  | "strong"
+  | "em"
+  | "label"
+  | "blockquote"
+  | "h1"
+  | "h2"
+  | "h3"
+  | "h4"
+  | "h5"
+  | "h6";
+
+type PolymorphicRef<T extends ElementType> = ComponentPropsWithRef<T>["ref"];
+
+export type TypographyProps<T extends TypographyElement = "p"> = {
+  as?: T;
+  role?: TypographyRole;
+  children?: React.ReactNode;
+  className?: string;
+} & ComponentPropsWithoutRef<T>;
+
+const Typography = forwardRef(
+  <T extends TypographyElement = "p">(
+    {
+      as,
+      role = "textMdRegular",
+      children,
+      className,
+      ...props
+    }: TypographyProps<T>,
+    ref: PolymorphicRef<T>
+  ) => {
+    const Component = (as ?? "p") as ElementType;
+    const classNames = [typographyRecipe({ role }), className]
+      .filter(Boolean)
+      .join(" ");
+    return (
+      <Component ref={ref} className={classNames} {...props}>
+        {children}
+      </Component>
+    );
+  }
+);
+
+Typography.displayName = "Typography";
+
+export { Typography };
