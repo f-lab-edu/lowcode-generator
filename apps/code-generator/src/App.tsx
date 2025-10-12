2de2c0c8a5c502ae5f04a6a3ead4b7b08df49984
Tessa1217 <kwonyuj1996@gmail.com>
2025-10-13 02:48:59 +0900

    feat(component palette): Code generator app에 registry 이용하여 컴포넌트 팔레트 1차 생성

--- apps/code-generator/src/App.tsx
+++ apps/code-generator/src/App.tsx
@@ -1,45 +1,17 @@
-import { useState } from "react";
-import { Button } from "@packages/ui";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { ComponentPalette } from "./components/component-palette";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <div className="bg-slate-50">
-        <Button color="info" size="sm">
-          Info 버튼
-        </Button>
-        <Button color="brand">Brand 버튼</Button>
-        <Button color="danger" size="lg">
-          Danger 버튼
-        </Button>
-        <Button color="warning" size="md">
-          Warning 버튼
-        </Button>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="app-layout">
+      <div className="sidebar">
+        <div className="sidebar-header">
+          <h2>Components</h2>
+        </div>
+        <ComponentPalette />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <div>Canvas 영역</div>
+    </div>
   );
 }
 
